f4afb87eaabf152b1966008e8aedfa7cfdf4c4ec
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-10-24 10:50:03 -0500

    Add the navbar,navBar'logo and the background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header";
 import { NavBar } from "./components/NavBar";
 
 const App = () => {
   return (
     <>
         <NavBar />
+        <Header/>
     </>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import "../sass/layout/navbar.scss";
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <>
+      <nav className="NavBar">
+          <div className="NavBar__logo">
+            <span className="NavBar__logo-circle"></span>
+            <span className="NavBar__logo-circle2"></span>
+          </div>
+
+          <div className="NavBar__items">
+            <div className="NavBar__item NavBar__item-1">
+              <span>00</span>
+              <span>home</span>
+            </div>
+            <div className="NavBar__item NavBar__item-2">
+              <span>01</span>
+              <span>destination</span>
+            </div>
+            <div className="NavBar__item NavBar__item-3">
+              <span>02</span>
+              <span>crew</span>
+            </div>
+            <div className="NavBar__item NavBar__item-4">
+              <span>03</span>
+              <span>technology</span>
+            </div>
+          </div>
+      </nav>
+    </>
+  );
+};
