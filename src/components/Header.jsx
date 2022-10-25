4d1aa7d5308b0cd1e7439ee53a457e3cc1eb1958
fernando-ui <fst-96@hotmail.com>
2022-10-24 20:12:40 -0500

    Add the contento of the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,22 @@ import "../sass/layout/header.scss";
 export const Header = () => {
   return (
     <header className="header">
-      <img
-        src={getImageUrl("home/background-home-desktop.jpg")}
-        alt="background-home-desktop.jpg"
-        className="header__image"
-      />
-      <div>
+      <div className="header__img">
         <div className="header__description">
-          <div className="header__description-1">
-            so, you wanto to travel to
+          <div className="header__container">
+            <div className="header__description-1">
+              so, you wanto to travel to
+            </div>
+            <div className="header__title">space</div>
+            <div className="header__description-2">
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </div>
           </div>
-          <div className="header__title">space</div>
-          <div className="header__description-2">
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </div>
+          <div className="header__circle">explore</div>
         </div>
-        <div className="header__circle">explore</div>
       </div>
     </header>
   );
