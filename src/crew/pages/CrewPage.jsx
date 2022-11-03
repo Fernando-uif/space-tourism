6421a80f6444b31a1def1d92fba699da28359e9b
fernando-ui <fst-96@hotmail.com>
2022-11-02 21:48:32 -0600

    Add the principal page on crewPage

--- src/crew/pages/CrewPage.jsx
+++ src/crew/pages/CrewPage.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import "../../sass/pages/crew/crew.scss";
 import { NavCrew } from "../../ui/components/NavCrew";
+import { CrewOne } from "./CrewOne";
 
 export const CrewPage = () => {
+  const [location, setLocation] = useState(`${window.location.origin}/crew/`);
   return (
     <section className="crew">
       <div className="crew__img">
         <div className="crew__title">
           <span className="crew__number">02</span> Meet your crew
         </div>
-        <NavCrew />
+        <NavCrew setLocation={setLocation} />
+        {location === `${window.location.origin}/crew/` && <CrewOne />}
       </div>
     </section>
   );
--- src/ui/components/NavCrew.jsx
+++ src/ui/components/NavCrew.jsx
@@ -1,6 +1,28 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "../../sass/layout/navCrew.scss";
-export const NavCrew = () => {
+export const NavCrew = ({ setLocation }) => {
+  const chageHref = (e) => {
+    const classElement = e.target.classList[1];
+    const number = classElement.split("-")[1];
+    switch (number) {
+      case "1":
+        setLocation(`${window.location.origin}/crew/`);
+        break;
+      case "2":
+        setLocation(`${window.location.origin}/crew/crew-two`);
+        break;
+      case "3":
+        setLocation(`${window.location.origin}/crew/crew-three`);
+        break;
+      case "4":
+        setLocation(`${window.location.origin}/crew/crew-four`);
+        break;
+
+      default:
+        setLocation(`${window.location.origin}/crew/`);
+        break;
+    }
+  };
   return (
     <>
       <div className="navCrew">
@@ -9,7 +31,9 @@ export const NavCrew = () => {
             "navCrew__item navCrew__item-1 " +
             (isActive ? "navCrew__selected" : "")
           }
-          to="crew-one"
+          to="/crew/"
+          onClick={(e) => chageHref(e)}
+          end
         ></NavLink>
         <NavLink
           className={({ isActive }) =>
@@ -17,6 +41,7 @@ export const NavCrew = () => {
             (isActive ? "navCrew__selected" : "")
           }
           to="crew-two"
+          onClick={(e) => chageHref(e)}
         ></NavLink>
         <NavLink
           className={({ isActive }) =>
@@ -24,6 +49,7 @@ export const NavCrew = () => {
             (isActive ? "navCrew__selected" : "")
           }
           to="crew-three"
+          onClick={(e) => chageHref(e)}
         ></NavLink>
         <NavLink
           className={({ isActive }) =>
@@ -31,9 +57,8 @@ export const NavCrew = () => {
             (isActive ? "navCrew__selected" : "")
           }
           to="crew-four"
-        >
-          {" "}
-        </NavLink>
+          onClick={(e) => chageHref(e)}
+        ></NavLink>
       </div>
       <Outlet />
     </>
